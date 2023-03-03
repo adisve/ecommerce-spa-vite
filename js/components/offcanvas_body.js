1a3f1d23f0e13270a045d299dddd618176daee83
Adis Veletanlic <adis.veletanlic@gmail.com>
2023-03-03 23:15:50 +0100

    Add total price to offcanvas

--- js/components/offcanvas_body.js
+++ js/components/offcanvas_body.js
@@ -1,46 +1,50 @@
-import { convertBooksToCartItems } from "../utils/book_utils";
-import { store } from "../store/store";
+import { convertBooksToCartItems, getTotalPrice } from "../utils/book_utils";
 
 export function renderOffcanvasBody(state) {
   const offcanvasBody = document.querySelector('#offcanvas-body');
-  offcanvasBody.innerHTML = state.shoppingCart.length > 0 ? convertBooksToCartItems(state.shoppingCart).map(cartItem => `
-    <div class="d-flex align-items-center justify-content-between cart-item-container mb-3">
-      <div class="d-flex align-items-center">
-        <div>
-          <h5 class="mb-0">${cartItem.title}</h6>
-          <p style="font-size: 17px;" class="mb-1">${cartItem.author}</p>
-          <div class="d-flex align-items-center">
-            <i class="fas fa-hashtag me-1"></i>
-            <span class="me-3">${cartItem.numberOfBooksWithTitle}</span>
-            <i class="fas fa-dollar-sign me-1"></i>
-            <span>${cartItem.sum}</span>
+
+  if (state.shoppingCart.length > 0) {
+    const cartItemsHtml = convertBooksToCartItems(state.shoppingCart).map(cartItem => `
+      <div class="d-flex align-items-center justify-content-between cart-item-container mb-3">
+        <div class="d-flex align-items-center">
+          <div>
+            <h5 class="mb-0">${cartItem.title}</h6>
+            <p style="font-size: 17px;" class="mb-1">${cartItem.author}</p>
+            <div class="d-flex align-items-center">
+              <i class="fas fa-hashtag me-1"></i>
+              <span class="me-3">${cartItem.numberOfBooksWithTitle}</span>
+              <i class="fas fa-dollar-sign me-1"></i>
+              <span>${cartItem.sum}</span>
+            </div>
           </div>
         </div>
       </div>
-      <button type="button" class="btn-close" aria-label="Remove item" data-book-id="${cartItem.id}"></button>
-    </div>
-    <hr>
-  `).join('') : `<h5>You don't have any items in your shopping cart</h5>`;
+      <hr>
+    `).join('');
 
-  // Add event listeners to remove items from cart
-  const removeBtns = offcanvasBody.querySelectorAll('.btn-close');
-    removeBtns.forEach((btn) => {
-      btn.addEventListener('click', (event) => {
-        const bookId = parseInt(event.target.dataset.bookId);
-        const newState = {
-          ...state,
-          shoppingCart: state.shoppingCart.filter((book) => book.id !== bookId),
-        };
-        store.setState(newState);
-      });
-    });
+    const totalPrice = getTotalPrice(state.shoppingCart);
 
-  // Add checkout button
-  const checkoutBtn = document.createElement('button');
-  checkoutBtn.classList.add('btn', 'btn-primary', 'checkout-btn');
-  checkoutBtn.innerText = 'Checkout';
-
-  const offcanvasContent = document.querySelector('.offcanvas-body');
-  offcanvasContent.appendChild(checkoutBtn);
+    offcanvasBody.innerHTML = `
+      <div class="d-flex justify-content-between align-items-center mb-3">
+        <h5 class="mb-0">Total price:</h5>
+        <span class="ms-3">$${totalPrice.toFixed(2)}</span>
+      </div>
+      <hr/>
+      ${cartItemsHtml}
+      <div class="d-flex align-items-center">
+        <button type="button" class="btn btn-primary checkout-btn d-flex align-items-center">
+          <i class="fas fa-credit-card me-2"></i>Checkout
+        </button>
+      </div>
+    `;
+  } else {
+    offcanvasBody.innerHTML = `
+      <h5>You don't have any items in your shopping cart</h5>
+      <div class="d-flex align-items-center">
+        <button type="button" class="btn btn-primary checkout-btn d-flex align-items-center">
+          <i class="fas fa-credit-card me-2"></i>Checkout
+        </button>
+      </div>
+    `;
+  }
 }
-
